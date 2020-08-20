506c0ad0307aca2461e88d2fcfa12ca1b736b9ac
yoko-kaneko <yoko@cybozu.co.jp>
2020-08-20 18:21:06 +0900

    Deleted eslintrc

--- test-01/source/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: "@cybozu/eslint-config/presets/kintone-customize-es5",
-    "env": {"browser": true},
-    "globals": {
-        "kintoneUIComponent": false,
-        "KintoneRestAPIClient": false
-    }
-};
